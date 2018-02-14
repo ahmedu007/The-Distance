cbc54d29942302959bb6af990edb128f41573725
ahmedu007 <umair170@hotmail.com>
2018-02-14 21:39:40 +0000

    Adding hover effect

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -3,29 +3,34 @@ import { Card, Icon, Image } from "semantic-ui-react";
 import * as moment from "moment";
 import { Link } from "react-router-dom";
 
-const EventCard = props => (
-  <Card raised centered>
-    <Link to={`/events/${props.event.id}`}>
-      <Image src={props.event.logo.url} />
-      <Card.Content>
-        <Card.Header>{props.event.name.text}</Card.Header>
-        <br />
-        <Card.Meta style={{ textAlign: "left" }}>
-          <span className="date">
-            <Icon name="calendar" />
-            {moment(props.event.start.local).format("DD/MM/YYYY")}
-          </span>
-        </Card.Meta>
-        <Card.Description>{}</Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <span>
-          <Icon name="user" />
-          22 Friends
-        </span>
-      </Card.Content>
-    </Link>
-  </Card>
-);
+const EventCard = props => {
+  const location = props.event.start.timezone.split("/");
+  return (
+    <Card
+      raised
+      centered
+      style={{ textAlign: "left", minHeight: "350px" }}
+      id="img-zoom-in"
+    >
+      <Link to={`/events/${props.event.id}`}>
+        <Image src={props.event.logo.url} style={{ minHeight: "145px" }} />
+        <Card.Content style={{ alignItems: "justify" }}>
+          <Card.Header>{props.event.name.text}</Card.Header>
+          <br />
+          <Card.Meta>
+            <span className="date" style={{ fontSize: "14px" }}>
+              <Icon name="calendar" />
+              {moment(props.event.start.local).format("DD/MM/YYYY")}
+            </span>
+          </Card.Meta>
+          <Card.Description>
+            <Icon name="location arrow" />
+            {location[1]}
+          </Card.Description>
+        </Card.Content>
+      </Link>
+    </Card>
+  );
+};
 
 export default EventCard;
