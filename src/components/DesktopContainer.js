0805f9b17020526d304fbab161e7eafe0149cce8
ahmedu007 <umair170@hotmail.com>
2018-02-14 20:40:57 +0000

    Removing unnecessary imports

--- src/components/DesktopContainer.js
+++ src/components/DesktopContainer.js
@@ -2,16 +2,9 @@ import React, { Component } from "react";
 import {
   Button,
   Container,
-  Divider,
-  Grid,
-  Header,
-  Icon,
-  Image,
-  List,
   Menu,
   Responsive,
   Segment,
-  Sidebar,
   Visibility
 } from "semantic-ui-react";
 import HomepageHeading from "./HomepageHeading";
