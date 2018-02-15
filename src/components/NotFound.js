15dbd9008309e5976dbc43fc95886d28f94d6a06
ahmedu007 <umair170@hotmail.com>
2018-02-15 17:29:28 +0000

    Adding 404

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => (
+  <div style={{ textAlign: "center", marginTop: "15%" }}>
+    <h3>404 page not found</h3>
+    <p>
+      We are sorry but the page you are looking for <em>does not</em> exist.
+    </p>
+  </div>
+);
+
+export default NotFound;
