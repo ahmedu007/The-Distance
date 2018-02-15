41ec0391975fc60792094d244beba3bb740f8c17
ahmedu007 <umair170@hotmail.com>
2018-02-15 02:02:59 +0000

    Adding buttons on mobile view

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,88 @@
 import React from "react";
 import "./App.css";
 import EventCard from "./EventCard";
-import { Container, Pagination, Segment } from "semantic-ui-react";
+import { Container, Pagination, Button } from "semantic-ui-react";
 import Navbar from "./Navbar";
 
 class App extends React.Component {
   constructor() {
     super();
     this.handleChange = this.handleChange.bind(this);
+    this.handleBackClick = this.handleBackClick.bind(this);
+    this.handleNextClick = this.handleNextClick.bind(this);
+    this.state = {
+      activePage: 1
+    };
   }
 
   handleChange(event, { activePage }) {
     event.preventDefault();
+    this.setState({
+      activePage
+    });
     this.props.getEvents(activePage);
   }
 
+  handleBackClick(event) {
+    event.preventDefault();
+    let activePage = this.state.activePage - 1;
+    this.props.getEvents(activePage);
+    this.setState({
+      activePage
+    });
+  }
+
+  handleNextClick(event) {
+    event.preventDefault();
+    let activePage = this.state.activePage + 1;
+    this.props.getEvents(activePage);
+    this.setState({
+      activePage
+    });
+  }
+
   render() {
+    const { activePage } = this.props;
     return (
       <div>
         <Navbar fixed={true} button={false} />
         <br />
-        <Segment textAlign="center">
+        <br />
+        <br />
+        <Container style={{ textAlign: "center" }}>
           <Pagination
             defaultActivePage={1}
             firstItem={null}
             lastItem={null}
+            activePage={activePage}
             pointing
             secondary
             onPageChange={this.handleChange}
             totalPages={this.props.pageCount || 20}
+            id="pagination"
           />
-          <Container fluid style={{ width: "667px" }}>
+          <div id="navigation">
+            <Button
+              content="Previous"
+              icon="left arrow"
+              labelPosition="left"
+              onClick={this.handleBackClick}
+            />
+            <Button
+              content="Next"
+              icon="right arrow"
+              labelPosition="right"
+              onClick={this.handleNextClick}
+            />
+          </div>
+          <Container id="cards">
             <br />
             <br />
             {this.props.events.map((event, i) => (
               <EventCard event={event} key={i} />
             ))}
           </Container>
-        </Segment>
+        </Container>
       </div>
     );
   }
