59566fae5b83a1eca1db1e0595cb34c78696f197
ahmedu007 <umair170@hotmail.com>
2018-02-15 16:27:26 +0000

    Updating tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import renderer from "react-test-renderer";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App events={[]} />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test("renders the loading state if there are no events", () => {
+  const loading = renderer.create(<App loading={false} events={[]} />);
+  expect(loading).toMatchSnapshot();
 });
